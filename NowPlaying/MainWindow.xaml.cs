4016e4d0e647ae27d9c07065c31d913de4fd6ecc
naoey <naoey20@gmail.com>
2016-12-31 13:45:29 +0530

    Set up COM interface and get track info.

--- NowPlaying/MainWindow.xaml.cs
+++ NowPlaying/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Timers;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
+using iTunesLib;
 
 namespace NowPlaying
 {
@@ -20,9 +17,40 @@ namespace NowPlaying
     /// </summary>
     public partial class MainWindow : Window
     {
+        private iTunesApp app;
+        private Timer timer;
+
+        public String Identifier;
+
         public MainWindow()
         {
             InitializeComponent();
+            Identifier = "Hue";
+
+            Process[] plist = Process.GetProcessesByName("iTunes");
+
+            if (plist.Length > 0)
+            {
+                app = new iTunesApp();
+            }
+
+            Load();
+        }
+
+        public void Load()
+        {
+            if (app != null)
+            {
+                string tmpFile = System.IO.Path.GetTempFileName();
+                app.CurrentTrack?.Artwork[1].SaveArtworkToFile(tmpFile);
+                BitmapImage artwork = new BitmapImage();
+                artwork.BeginInit();
+                artwork.UriSource = new Uri(tmpFile);
+                artwork.EndInit();
+
+                TrackLabel.Content = String.Format("{0} - {1}", app.CurrentTrack?.Artist, app.CurrentTrack?.Name);
+                TrackArtwork.Source = artwork;
+            }
         }
     }
 }
